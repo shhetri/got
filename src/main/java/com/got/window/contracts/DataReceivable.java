5d344a8db96d508fdbab867d06ef08cbc2a17a1f
Sumit Chhetri <sumit955188@gmail.com>
2017-04-15 18:08:34 -0500

    complete window utility
    - singleton pattern
    - builder pattern
    - facotry method pattern
    - string helper created

---
+++ src/main/java/com/got/window/contracts/DataReceivable.java
@@ -0,0 +1,5 @@
+package com.got.window.contracts;
+
+public interface DataReceivable {
+    void receive(Object data);
+}
---
+++ src/main/java/com/got/window/contracts/WindowInterface.java
@@ -0,0 +1,7 @@
+package com.got.window.contracts;
+
+import javafx.stage.Stage;
+
+public interface WindowInterface {
+    Stage open();
+}
