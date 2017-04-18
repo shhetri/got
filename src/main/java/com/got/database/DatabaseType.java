e904591db05bcd154b9c60f2b499b37d2d8ab3b7
Sulav Neupane <sulavneupane@gmail.com>
2017-04-18 13:43:31 -0500

    Database complete

---
+++ src/main/java/com/got/database/DatabaseType.java
@@ -0,0 +1,7 @@
+package com.got.database;
+
+public enum DatabaseType {
+
+    MYSQL, DERBY
+
+}
--- src/main/java/com/got/helpers/StringHelper.java
+++ src/main/java/com/got/helpers/StringHelper.java
@@ -6,4 +6,29 @@ public class StringHelper {
 
         return stripTrailingSlash(string.substring(0, string.length() - (string.endsWith("/") ? 1 : 0)));
     }
+
+    public static String camelCase(String string) {
+        StringBuilder sb = new StringBuilder();
+        boolean capitalizeNext = false;
+        for (char c:string.toCharArray()) {
+            if (c == '_') {
+                capitalizeNext = true;
+            } else {
+                if (capitalizeNext) {
+                    sb.append(Character.toUpperCase(c));
+                    capitalizeNext = false;
+                } else {
+                    sb.append(c);
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    public static String generateSetter(String string) {
+        string = camelCase(string);
+        string = "set" + Character.toUpperCase(string.charAt(0)) + string.substring(1, string.length());
+
+        return string;
+    }
 }
