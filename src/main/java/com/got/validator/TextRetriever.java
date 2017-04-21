8dce7b057615d00ce974b192af9a42b086ea28ad
Sumit Chhetri <sumit955188@gmail.com>
2017-04-21 11:30:32 -0500

    add date support in text receiver inside validator

--- src/main/java/com/got/validator/TextRetriever.java
+++ src/main/java/com/got/validator/TextRetriever.java
@@ -2,19 +2,27 @@ package com.got.validator;
 
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Control;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class TextRetriever {
 
     public static String retrieve(Control control) {
 
-        if(control instanceof TextField) {
+        if (control instanceof TextField) {
             return ((TextField) control).getText();
         } else if (control instanceof ComboBox) {
-            return (String)((ComboBox) control).getSelectionModel().getSelectedItem();
+            return (String) ((ComboBox) control).getSelectionModel().getSelectedItem();
+        } else if (control instanceof DatePicker) {
+            LocalDate date = ((DatePicker) control).getValue();
+            if (date != null) {
+                return date.format(DateTimeFormatter.ofPattern("MM-dd-yyyy"));
+            }
         }
 
         return null;
     }
-
 }
